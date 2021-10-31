f109a3a68f883adc337c0b2fb76b03c454c73fdf
Наталья Фёдорова <natalyafedorova5@gmail.com>
2021-10-31 15:59:16 +0300

    Добавьте файлы проекта.

---
+++ Hwork16.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+
+namespace Hwork16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double max = 0;
+            string Name = "";
+            string path = @"D:\!ОБУЧЕНИЕ\Проекты\Задание 16\Hwork16\Hwork16\bin\Debug\Products.json";
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            Product [] array = JsonSerializer.Deserialize<Product[]>(File.ReadAllText(path));
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Код: {array[i].CodeProduct}\nНаименование: {array[i].NameProduct}\nЦена: {array[i].PriceProduct}");
+                Console.WriteLine("-----------------------------------------------------");
+                if (array[i].PriceProduct > max)
+                {
+                    max = array[i].PriceProduct;
+                    Name = array[i].NameProduct;
+                }
+            }
+            Console.WriteLine($"\n");
+            Console.WriteLine($"Самый дорогой товар - {Name}, цена которого составляет {max:f2}");
+            Console.ReadKey();
+        }
+        class Product
+        {
+            [JsonPropertyName("Код")]
+            public int CodeProduct { get; set; }
+            [JsonPropertyName("Наименование")]
+            public string NameProduct { get; set; }
+            [JsonPropertyName("Цена")]
+            public float PriceProduct { get; set; }
+        }
+    }
+}
---
+++ Hwork16/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+
+namespace Hwork16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Products.json";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            Console.WriteLine("Введите:\n1.Код товара\n2.Название товара\n3.Цена товара");
+            const int n = 5;
+            Product[] array = new Product[5];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = new Product();
+                Console.WriteLine($"Введите данные по товару № {i + 1}");
+                array[i].inputProduct();
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                string jsonString = JsonSerializer.Serialize(array, options);
+                sw.WriteLine(jsonString);
+            }
+            Console.ReadKey();
+        }
+        class Product
+        {
+            [JsonPropertyName("Код")]
+            public int CodeProduct { get; set; }
+            [JsonPropertyName("Наименование")]
+            public string NameProduct { get; set; }
+            [JsonPropertyName("Цена")]
+            public float PriceProduct { get; set; }
+
+            public void inputProduct()
+            {
+                Console.Write("1.");
+                CodeProduct = Convert.ToInt32(Console.ReadLine());
+                Console.Write("2.");
+                NameProduct = Convert.ToString(Console.ReadLine());
+                Console.Write("3.");
+                PriceProduct = (float)Convert.ToDouble(Console.ReadLine());
+            }
+        }
+    }
+}
